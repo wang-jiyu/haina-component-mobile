5cb0e190c930e0cc6435d27164d76dce6130652f
sunlandong <sunlandong@9086.cn>
2018-01-24 18:26:09 +0800

    'loading组件'

---
+++ components/loading/index.tsx
@@ -0,0 +1,59 @@
+
+
+import * as React from 'react'
+import './loading.scss'
+interface ILoadingProps {
+    size?: number
+}
+export default class Loading extends React.Component<ILoadingProps, any>{
+
+    static defaultProps = {
+        size: 40
+    }
+
+    state = {
+        balde: 12,
+        style: {}
+    }
+
+    constructor(props){
+        super(props)
+    }
+    componentDidMount(){
+        let value = this.props.size
+        this.setStyle({
+            width: value,
+            height: value
+        })
+    }
+    componentWillReceiveProps(nextProps) {
+        let value = 40
+        if (!nextProps.size) {
+            value = 40
+        } else {
+            value = nextProps.size
+        }
+        this.setStyle({
+            width: value,
+            height: value
+        })
+    }
+    setStyle(style) {
+        this.setState({
+            style
+        })
+    }
+    render() {
+        const { balde, style } = this.state
+        return (
+            <div className="haina-loading">
+                <span className="haina-loading-spinners" style={style}>
+                    {Array.from(new Array(balde), (el, index) => {
+                        return <i key={index} className="haina-loading-spinner"></i>
+                    })}
+                </span>
+            </div>
+        )
+    }
+
+}
